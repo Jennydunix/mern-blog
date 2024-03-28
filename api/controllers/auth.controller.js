1aa3534ba487113edf6ac00f5756ae53c54f7a3e
jennydunix <jennifer.o.odunze@gmail.com>
2024-03-28 23:24:35 +0100

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,11 +14,11 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-   next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, "All fields are required"));
   }
 
-    // hash password
-    const hashPassword = bcryptjs.hashSync(password, 10);
+  // hash password
+  const hashPassword = bcryptjs.hashSync(password, 10);
 
   // create new user
   const newUser = new User({
@@ -34,5 +35,37 @@ export const signup = async (req, res, next) => {
     res.json("Signup successful");
   } catch (err) {
     next(err);
+  }
+};
+
+// create sign in API route
+export const signin = async (req, res, next) => {
+  // get username and password
+  const { email, password } = req.body;
+
+  // check if there's no username or password or both are null state an error if that's the case
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    // Check the password of the user
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+    // If both password and user are valid, authenticate the user
+    // creating a token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // hide hashed password
+    const {password: pass, ...rest} = validUser._doc;
+    // add token to cookie
+    res.status(200).cookie("access_token", token, {
+      httpOnly: true}).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import {signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+// Define a route to handle HTTP POST requests to the '/signup' endpoint, invoking the 'signup' function
 router.post('/signup', signup);
+// Define a route to handle HTTP POST requests to the '/signin' endpoint, invoking the 'signin' function
+router.post('/signin', signin);
+
 
 export default router;
