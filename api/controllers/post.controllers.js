5b89afa4c152c0c1ad5c0f83245aff564792e80b
jennydunix <jennifer.o.odunze@gmail.com>
2024-04-10 23:14:17 +0100

    create get posts api route

--- api/controllers/post.controllers.js
+++ api/controllers/post.controllers.js
@@ -29,3 +29,55 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+// get post
+export const getposts = async (req, res, next) => {
+  try {
+    // get a specific number of posts
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    // set a limit for the number of posts
+    const limit = parseInt(req.query.limit) || 9;
+    // sort direction(if its 1 mongodb will show ascending else otherwise)
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+         
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      })
+    }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+    // get the total number of posts
+    const totalPosts = await Post.countDocuments();
+
+    // get total number of posts in previous month
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    // calculate last month's post
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controllers.js';
+import { create, getposts } from '../controllers/post.controllers.js';
 
 // create a router based on express
 const router = express.Router();
 
 // create a post request
 router.post('/create', verifyToken, create);
+// create get post api route
+router.get('/getposts', getposts);
 
 export default router;
