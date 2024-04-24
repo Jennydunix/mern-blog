dd237a107114210c118469745498e09db2d15778
jennydunix <jennifer.o.odunze@gmail.com>
2024-04-24 23:34:57 +0100

    edited pre installation

--- api/index.js
+++ api/index.js
@@ -46,10 +46,10 @@ app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
 // specify the static folder
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-api.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // Adding middleware
